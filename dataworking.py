6cbdc3b4f9bd12a4804136a760cb9a3750a7c1e2
Evdodima <evdodima@gmail.com>
2016-08-14 16:17:57 +0300

    Plot of price + predicted price added

--- dataworking.py
+++ dataworking.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import quandl as qd
 import numpy as np
+import datetime
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
 
 df = qd.get('WIKI/GOOGL')
 
@@ -15,7 +20,7 @@ forecast_col = 'Adj. Close'
 forecast_time = 30
 
 df['label'] = df[forecast_col].shift(-forecast_time)
-# df.fillna(-9999, inplace=True)
+# df.fillna(-9999, inplace=True) нахуй надо если мы потом все равно делаем dropna
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
@@ -34,3 +39,26 @@ accuracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
 
 print(accuracy, '\n', forecast_set)
+
+# если поймешь че тут дальше происходит расскажи мне
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+df['Forecast'] = np.nan
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix+=one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+
+plt.show()
+
