70f579784ee19c902d3c4c00f2ec1d9f2c566010
Evdodima <evdodima@gmail.com>
2016-08-17 22:10:35 +0300

    proxy added

--- parsing_Askapatient.py
+++ parsing_Askapatient.py
@@ -5,7 +5,7 @@ import random
 
 letters = string.ascii_uppercase[:]
 
-url = 'http://webcache.googleusercontent.com/search?q=cache:http://www.askapatient.com/drugalpha.asp?letter=%s'
+url = 'http://www.askapatient.com/drugalpha.asp?letter=%s'
 urls = [url % s for s in letters]
 
 
@@ -24,15 +24,18 @@ def LoadUserAgents(uafile='user_agents.txt'):
 
 
 user_agents = LoadUserAgents()
+proxies = ['124.133.240.88:8080', '201.55.46.6:80', '210.101.131.231:8080', '84.28.221.68:80']
 for url in urls:
-    print ('parsing '+url[-1])
+    print('parsing ' + url[-1])
+    proxy = random.choice(proxies)
     headers = {
         'user-agent': random.choice(user_agents),
-        "Connection": "close"
+        "Connection": "close",
+        'Cookie': '__utmz=183609910.1471172088.1.1.utmccn=(referral)|utmcsr=google.ru|utmcct=/|utmcmd=referral; D_SID=178.204.169.19:ID1F7DHRP6ht486AIYwzR2IhAOQfgUTQGEFLSSXeXBA; ASPSESSIONIDSQQASABD=GCNIDKCBGNHPANIIEBJEMHAF; ASPSESSIONIDSQQBQCBC=GILLBGPBDNEMPKDDCLNAMDAG; __utma=183609910.1338363169.1471172088.1471450935.1471454680.8; __utmc=183609910; D_ZID=8D3FB4D9-86DF-3264-82D0-668C3F6A67EE; D_ZUID=02E07275-D545-34AF-8D38-C3ACAB8E30DE; __utmb=183609910; __atuvc=24%7C33; __atuvs=57b49dd7766c2847002; D_PID=F3F1AE20-D4CC-33C3-854A-1209E349330F; D_IID=B1C87246-9BAF-3781-BEE4-5476591A3FE5; D_UID=B4B3E55C-D8D3-37AD-B93F-109962C5AE57; D_HID=qhMWGl7HlD7aRi4u8LUWqHMzjnlB5iHb0l3s0PJ1wxc'
     }
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, proxies={'http': proxy}, headers=headers)
     page = html.fromstring(r.text)
-    print(html.tostring(page.cssselect('title').pop()))
+    print(html.tostring(page))
 
 # alpha_pages = [requests.get(url) for url in urls]
 # alpha_pages = [html.document_fromstring(page.text) for page in alpha_pages]
