57c3a233b6ea29da4e4b553405fc75fbfba097aa
Evdodima <evdodima@gmail.com>
2016-08-17 21:44:58 +0300

    useragents added

--- parsing_Askapatient.py
+++ parsing_Askapatient.py
@@ -1,13 +1,40 @@
 import string
 import lxml.html as html
 import requests
+import random
 
 letters = string.ascii_uppercase[:]
 
 url = 'http://webcache.googleusercontent.com/search?q=cache:http://www.askapatient.com/drugalpha.asp?letter=%s'
 urls = [url % s for s in letters]
 
-alpha_pages = [requests.get(url) for url in urls]
-alpha_pages = [html.document_fromstring(page.text) for page in alpha_pages]
 
-print(html.tostring(alpha_pages[0]))
+def LoadUserAgents(uafile='user_agents.txt'):
+    """
+    uafile : string
+        path to text file of user agents, one per line
+    """
+    uas = []
+    with open(uafile, 'rb') as uaf:
+        for ua in uaf.readlines():
+            if ua:
+                uas.append(ua.strip()[1:-1 - 1])
+    random.shuffle(uas)
+    return uas
+
+
+user_agents = LoadUserAgents()
+for url in urls:
+    print ('parsing '+url[-1])
+    headers = {
+        'user-agent': random.choice(user_agents),
+        "Connection": "close"
+    }
+    r = requests.get(url, headers=headers)
+    page = html.fromstring(r.text)
+    print(html.tostring(page.cssselect('title').pop()))
+
+# alpha_pages = [requests.get(url) for url in urls]
+# alpha_pages = [html.document_fromstring(page.text) for page in alpha_pages]
+
+# print(html.tostring(alpha_pages[0]))
