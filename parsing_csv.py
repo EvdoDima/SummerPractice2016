f93818be22aaa25e059a8b7f156ab120f00cafdc
Evdodima <evdodima@gmail.com>
2016-08-20 07:32:11 +0300

    reviews counting added

--- parsing_csv.py
+++ parsing_csv.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import os
+import time
 
-df = pd.DataFrame.from_csv('out/drugs/ACCOLATE.csv')
 
-print(df)
+def file_len(fname):
+    return len(pd.DataFrame.from_csv(fname))
 
+
+count = 1
+start = time.time()
+for i in os.listdir('out/drugs'):
+    count += file_len('out/drugs/'+i)
+end = time.time()
+print(count, end-start)
