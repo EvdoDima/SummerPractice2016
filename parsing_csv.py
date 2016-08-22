f3a393b548438a79f1c810ff450b215a2911a033
Evdodima <evdodima@gmail.com>
2016-08-23 00:44:47 +0300

    regex edited not to get numbers

--- parsing_csv.py
+++ parsing_csv.py
@@ -20,10 +20,10 @@ def parse_csv_dir(dir):
 
 def count_words(df):
     words = {}
-    df['Review'] = df['Comments']+' '+df['Side Effects']
+    df['Review'] = df['Comments'] + ' ' + df['Side Effects']
     for review in df['Review']:
         review = review.lower()
-        for word in re.sub("[^\w]|_", " ", review).split():
+        for word in re.sub("[^a-zA-Z]|_", " ", review).split():
             if word in words.keys():
                 words[word] += 1
             else:
@@ -36,17 +36,15 @@ def count_words(df):
     return sorted_words
 
 
-
-
 df = pd.DataFrame.from_csv('out/merged_input.csv', parse_dates=False)
 df = df.drop(['Date Added'], axis=1)
 df = df.dropna()
 
 # words = count_words(df)
 # words_df = pd.DataFrame(words)
+# words_df.columns = ['Word', 'Count']
 # words_df.to_csv('out/words.csv')
 
 words_from = pd.DataFrame.from_csv('out/words.csv')
-
 
 print(words_from.head())
