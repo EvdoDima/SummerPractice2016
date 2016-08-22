b22be3cfee7107015585d02bd2286e45eec4f9b1
Evdodima <evdodima@gmail.com>
2016-08-22 23:39:04 +0300

    parsing all csv into dataset, merged_input file added

--- parsing_csv.py
+++ parsing_csv.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import os
 import time
+import numpy as np
+
+input_dir = 'out/drugs/'
 
 
-def file_len(fname):
-    return len(pd.DataFrame.from_csv(fname))
+def parse_csv_dir(dir):
+    start = time.time()
+    df = pd.DataFrame()
+    for i in os.listdir(dir):
+        print(i)
+        df = df.append(pd.DataFrame.from_csv(input_dir + i))
+    end = time.time()
+    print(end - start)
+    return df
 
 
-count = 1
-start = time.time()
-for i in os.listdir('out/drugs'):
-    count += file_len('out/drugs/'+i)
-end = time.time()
-print(count, end-start)
+df = pd.DataFrame.from_csv('out/merged_input.csv',parse_dates=False)
+df = df.drop(['Date Added','Rating'], axis=1)
+# df = df.dropna()
+shuffled_df = df.iloc[np.random.permutation(len(df))]
+
+print(shuffled_df)
