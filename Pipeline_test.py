d8f52b2245c4190ef806c985480e2c9f42a1d3d7
Evdodima <evdodima@gmail.com>
2016-08-29 01:51:03 +0300

    drug statistic parsing added (WIP)

--- Pipeline_test.py
+++ Pipeline_test.py
@@ -1,28 +1,51 @@
+from sklearn.cross_validation import KFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import pandas as pd
-import numpy as np
 from sklearn.metrics import classification_report
 from sklearn import linear_model, cross_validation, svm
 from sklearn import metrics
 from sklearn import cross_validation
 from sklearn import linear_model, preprocessing
 import os, re, functools
+import numpy as np
 
 
-def parse_csv_dir(dir):
-    data = pd.DataFrame()
+def parse_csv_dir_to_drugs_stat(dir):
+    data = pd.DataFrame(columns=['Name', 'Reviews count', 'F', 'M'])
+    ind = 0
+
+    def process_drug(drug_data):  # Drug processing. remove one gendered drug
+        drug_data.fillna("", inplace=True)
+        ln = len(drug_data)
+        female_count = len(drug_data[drug_data["Sex"] == "F"])
+
+        return ln, female_count, ln - female_count
     for i in os.listdir(dir):
         print(i)
+        drug = pd.DataFrame.from_csv(dir + i)
+        data.loc[ind] = ([i] + list(process_drug(drug)))
+        ind += 1
+    data.sort_values('Reviews count', inplace=True, ascending=False)
+    return data
+
+
+def choose_drugs(drug_data):  # Drug processing. remove one gendered drug. get the top
+    return drug_data['Name'].values
+
+
+def parse_drugs():
+    dir = "out/drugs/"
+    data = pd.DataFrame.from_csv("out/drugs_stat.csv")
+    print("parsing drugs dir...")
+    for i in choose_drugs(data):
         new_data = pd.DataFrame.from_csv(dir + i)
-        new_data = process_drug(new_data)
         data = data.append(new_data)
     return data
 
 
-def process_drug(drug_data):  # Drug processing. remove one gendered drug
-    return drug_data
+
 
 
 def prepare_data(data):
@@ -47,29 +70,54 @@ def prepare_data(data):
     data['Review'] = data['Review'].apply(replace_non_letters)
     return data
 
-
-data = pd.DataFrame.from_csv("out/data.csv")
+drugs_stat = parse_csv_dir_to_drugs_stat("out/drugs/")
+drugs_stat.to_csv("drugs_stat.csv")
+data = parse_drugs()
+data.to_csv("out/data.csv")
+# data = pd.DataFrame.from_csv("out/data.csv")
 dataset = prepare_data(data)
 
-
-clf = Pipeline([('vect', TfidfVectorizer(ngram_range=(1,2))),
-                ('clf', linear_model.LogisticRegression(n_jobs=-1))
+clf = Pipeline([('vect', TfidfVectorizer(ngram_range=(1, 2), max_features=10000))
+                # ('clf', linear_model.SGDClassifier(penalty='l1',n_jobs=-1,alpha=0.0003))
+                   , ('clf', linear_model.LogisticRegression(n_jobs=-1))
                 ])
 
-y = dataset['Sex']
 X = dataset['Review']
+y = dataset['Sex']
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.1)
-clf.fit(X_train, y_train)
-
-print(clf.named_steps['vect'].get_feature_names())
-y_pred = clf.predict(X_test)
-report = metrics.classification_report(y_test, y_pred, target_names=["F", "M"])
-print(report)
-print(metrics.accuracy_score(y_test, y_pred))
+print("training...")
+# Train/test split
+# clf.fit(X, y)
+# print(len(clf.named_steps['vect'].get_feature_names()))
 
 
 # cross-validation
 # scores = cross_validation.cross_val_score(clf, X, y, cv=5)
-# print(scores)
-# print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+# K-fold CV
+k_fold = KFold(n=len(X), n_folds=10)
+scores = []
+for train_indices, test_indices in k_fold:
+    local_train_X = X.iloc[train_indices]
+    local_train_y = y.iloc[train_indices]
+
+    local_test_X = X.iloc[test_indices]
+    local_test_y = y.iloc[test_indices]
+
+    clf.fit(local_train_X, local_train_y)
+    predictions = clf.predict(local_test_X)
+
+    score = metrics.accuracy_score(local_test_y, predictions)
+    scores.append(score)
+    print(score)
+#
+scores = np.array(scores)
+
+
+
+
+y_pred = clf.predict(X)
+report = metrics.classification_report(y, y_pred, target_names=["F", "M"])
+print(report)
+print("CV accuracy: %0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2))
+print("Full corpus accuracy : %0.3f" % metrics.accuracy_score(y, y_pred))
